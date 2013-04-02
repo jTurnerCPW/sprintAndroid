f72f1a8688079c06a67b03924641e589be8486a4
michael.allon@compuware.com <michael.allon@compuware.com>
2013-04-02 13:59:02 -0400

    3rd commit

--- sprintAndroid/src/com/example/sprintandroid/MainActivity.java
+++ sprintAndroid/src/com/example/sprintandroid/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.sprintandroid;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		
+		Log.v("tag", "hi");
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
