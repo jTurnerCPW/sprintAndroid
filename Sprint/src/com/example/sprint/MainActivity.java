270423026dc7bdf04d703c4fa13c588287e4e783
Jackson Turner <jackson.turner@compuware.com>
2013-04-03 13:16:29 -0400

    preferences fragments and activities.  detects api level and redirects appropriately.

--- Sprint/src/com/example/sprint/MainActivity.java
+++ Sprint/src/com/example/sprint/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.sprint;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends Activity {
+
+	private static final int EDIT_ID = Menu.FIRST+2;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -14,20 +18,48 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 	}
 
+
+						//TODO:check with mike re: ABS - do will the menu work ok with sherlock and no inflate?
+						//if no, figure out how to change the "settings" menu option to my "edit prefs"
+	//	@Override
+	//	public boolean onCreateOptionsMenu(Menu menu) {
+	//		// Inflate the menu; this adds items to the action bar if it is present.
+	//		getMenuInflater().inflate(R.menu.main, menu);
+	//		return true;
+	//	}
+	//	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+		menu.add(Menu.NONE, EDIT_ID, Menu.NONE, "Edit Prefs")
+		.setIcon(R.drawable.misc)
+		.setAlphabeticShortcut('e');
+
+		return(super.onCreateOptionsMenu(menu));
 	}
 	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case EDIT_ID:
+			if (Build.VERSION.SDK_INT<Build.VERSION_CODES.HONEYCOMB) {
+				startActivity(new Intent(this, EditPreferences.class));
+			}
+			else {
+				startActivity(new Intent(this, EditPreferencesHC.class));
+			}
+			return(true);
+		}
+
+		return(super.onOptionsItemSelected(item));
+	}
+
 	public void startScanner(View view) {
 		Intent intent = new Intent(this, ScannerActivity.class);
 		startActivity(intent);
 
-		
+
 	}
-	
+
 	public void startPrinterListActivity(View view){
 		Intent intent = new Intent(this, PrinterListActivity.class);
 		startActivity(intent);
