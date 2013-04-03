f34d5ab66ff1e6d2eac7f5a296f1756c852e77da
amikxp0 <amikxp0@192.168.176.68>
2013-04-03 13:11:37 -0400

    klhlkj

---
+++ Sprint/src/com/example/sprint/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.sprint;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	public void startScanner(View view) {
+		Intent intent = new Intent(this, ScannerActivity.class);
+		startActivity(intent);	
+	}
+	
+	public void startPrinterListActivity(View view){
+		Intent intent = new Intent(this, PrinterListActivity.class);
+		startActivity(intent);
+	}
+	
+	public void startJobListActivity(View view){
+		Intent intent = new Intent(this, JobListActivity.class);
+		startActivity(intent);
+	}
+	
+	/*HELLLO CHANGES*/
+	/*Mikes changes*/
+	/* Mikes more changes */
+	/* Mikes more more changes */
+
+}
