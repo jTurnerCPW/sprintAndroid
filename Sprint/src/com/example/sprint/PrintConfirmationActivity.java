fa70cd332ceb7218cd6eb12e07b7a920692ca145
michael.allon@compuware.com <michael.allon@compuware.com>
2013-04-08 11:17:46 -0400

    added button functionality to confirmation page

--- Sprint/src/com/example/sprint/PrintConfirmationActivity.java
+++ Sprint/src/com/example/sprint/PrintConfirmationActivity.java
@@ -1,14 +1,20 @@
 package com.example.sprint;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class PrintConfirmationActivity extends ABSFragmentActivity{
 
+	private String printerName;
+	private String job_id;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_print_confirmation);
+		Intent intent = getIntent();
+		printerName = intent.getStringExtra("printer_name");
+		job_id = intent.getStringExtra("job_id");
 	}
 
 }
--- Sprint/src/com/example/sprint/PrintConfirmationFragment.java
+++ Sprint/src/com/example/sprint/PrintConfirmationFragment.java
@@ -4,19 +4,43 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
-public class PrintConfirmationFragment extends Fragment {
+public class PrintConfirmationFragment extends Fragment implements OnClickListener{
 
 	private LinearLayout view;
+	private Button bCancel;
+	private Button bPrint;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		
 		view = (LinearLayout) inflater.inflate(R.layout.fragment_print_confirmation, container, false);
+		bCancel = (Button) view.findViewById(R.id.bCancel);
+		bCancel.setOnClickListener(this);
+		bPrint = (Button) view.findViewById(R.id.bPrint);
+		bPrint.setOnClickListener(this);
 		return view;
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch(v.getId()){
+		
+		case R.id.bCancel:
+			//  Don't print the job.  Go back
+			getActivity().finish();
+			break;
+		case R.id.bPrint:
+			// Print the job.
+			break;
+		
+		}
+		
 	}
 	
 }
