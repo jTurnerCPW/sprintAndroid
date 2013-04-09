eb61fb745fc68a7849777dc9fa93ee46279a8d1c
amijzn0 <amijzn0@192.168.176.168>
2013-04-09 08:41:31 -0400

    Removed some old LOGs.

--- Sprint/src/com/example/sprint/PrinterListFragment.java
+++ Sprint/src/com/example/sprint/PrinterListFragment.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -41,7 +40,6 @@ public class PrinterListFragment extends Fragment implements OnItemClickListener
 		}
 
 		public void onTextChanged(CharSequence s, int start, int before, int count) {
-			Log.v("PrinterListActivity", "onTextChanged");
 			adapter.getFilter().filter(s);
 		}
 	};
