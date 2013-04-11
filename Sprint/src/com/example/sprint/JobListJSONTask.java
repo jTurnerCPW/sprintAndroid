b667bc73157c149147ed768291bda4dee7935369
amijzn0 <amijzn0@dtw125408m01.clients.nasa.cpwr.corp>
2013-04-11 16:08:53 -0400

    Added timeouts to the job and printer list tasks.

--- Sprint/src/com/example/sprint/JobListJSONTask.java
+++ Sprint/src/com/example/sprint/JobListJSONTask.java
@@ -13,6 +13,8 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -25,10 +27,12 @@ import android.content.DialogInterface.OnDismissListener;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.preference.PreferenceManager;
+import android.widget.Toast;
 
-public class JobListJSONTask extends AsyncTask<Context, Void, ArrayList<Job>> {
+public class JobListJSONTask extends AsyncTask<Context, Void, String> {
 	private JobListFragment jobListFragment;
 	ProgressDialog pd;
+	ArrayList<Job> jobs = new ArrayList<Job>();
 	
 	// Constructor for the JobListJSON Task
 	public JobListJSONTask(JobListFragment jobListFragment) {
@@ -36,12 +40,17 @@ public class JobListJSONTask extends AsyncTask<Context, Void, ArrayList<Job>> {
 	}
 	
 	@Override
-	protected ArrayList<Job> doInBackground(Context... params) {
+	protected String doInBackground(Context... params) {
 				
 		try{
 		    // Create a new HTTP Client and setup the POST
 		    DefaultHttpClient defaultClient = new DefaultHttpClient();
 		    HttpPost httpPostRequest = new HttpPost("http://10.24.16.122/show_active_jobs.php");
+		    
+		    // Set timeout for the connection
+		    final HttpParams httpParameters = defaultClient.getParams();
+		    HttpConnectionParams.setConnectionTimeout(httpParameters, 6000);
+		    HttpConnectionParams.setSoTimeout(httpParameters, 6000);
 		    
 		    // Grab the username from preferences
 		    SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(jobListFragment.getActivity());
@@ -87,34 +96,53 @@ public class JobListJSONTask extends AsyncTask<Context, Void, ArrayList<Job>> {
 				        
 				        jobList.add(new Job(id, name, user, type));
 				    }
-		      
-			        return jobList;
+				    
+				    // Assign the jobs
+				    jobs = jobList;
+				    
+			        return "Success";
 			    } catch (Exception e) {
 			    	e.printStackTrace();
 			    }
 
-			   return new ArrayList<Job>();
+			   return "Failed";
 		    }
 
-	        return new ArrayList<Job>();
+	        return "Failed";
 
-		} catch(Exception e){
-		    e.printStackTrace();
+		} catch(java.net.SocketTimeoutException e) {
+			return "Timeout";
+		} catch(Exception e) {
+			e.printStackTrace();  
 		}
 		
-        return new ArrayList<Job>();
+        return "Failed";
 	}
 	
 	@Override
-	protected void onPostExecute(ArrayList<Job> jobs) {
+	protected void onPostExecute(String result) {
+		
+		if(result.equals("Success"))
+		{
+			// Dynatrace Job List Load Leave
+			CompuwareUEM.leaveAction("Job List Load");
+		}
+		else if(result.equals("Timeout"))
+		{
+			// Timeout tell the user to check if they are connected to Compuware and retry
+			Toast.makeText(jobListFragment.getActivity(), "Timeout!", Toast.LENGTH_LONG).show();
+		}
+		else
+		{
+			// Error occurred most likely on the back end
+			Toast.makeText(jobListFragment.getActivity(), "Error - loading jobs!", Toast.LENGTH_LONG).show();
+		}
+		
 		// Set the jobs and show the list and dismiss the progress dialog
 		jobListFragment.setJobs(jobs);
 		jobListFragment.showList();
 		pd.dismiss();		
 		jobListFragment.notifyJobLoadComplete();
-		
-		// Dynatrace Job List Load Leave
-		CompuwareUEM.leaveAction("Job List Load");
 	}
 	
 	@Override
@@ -128,7 +156,7 @@ public class JobListJSONTask extends AsyncTask<Context, Void, ArrayList<Job>> {
 		pd = new ProgressDialog(jobListFragment.getActivity());
 		pd.setMessage("Loading Jobs ...");
 		pd.show();
-		final AsyncTask<Context, Void, ArrayList<Job>> task = this;
+		final AsyncTask<Context, Void, String> task = this;
 		pd.setOnDismissListener(new OnDismissListener() {
 			
 			@Override
