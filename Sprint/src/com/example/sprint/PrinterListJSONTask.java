ecd5b75ca160dcb309e7b6aec99cc517f2470692
Jackson Turner <jackson.turner@compuware.com>
2013-04-12 10:31:56 -0400

    changed printer list load UEM action tag to specify android

--- Sprint/src/com/example/sprint/PrinterListJSONTask.java
+++ Sprint/src/com/example/sprint/PrinterListJSONTask.java
@@ -106,7 +106,7 @@ public class PrinterListJSONTask extends AsyncTask<Context, Void, String> {
 		if(result.equals("Success"))
 		{
 			// Dynatrace Printer List Load Leave
-			CompuwareUEM.leaveAction("Printer List Load");
+			CompuwareUEM.leaveAction("Android Printer List Load");
 		}
 		else if(result.equals("Timeout"))
 		{
@@ -129,7 +129,7 @@ public class PrinterListJSONTask extends AsyncTask<Context, Void, String> {
 	@Override
 	protected void onPreExecute() {
 		// Dynatrace Printer List Load Enter
-		CompuwareUEM.enterAction("Printer List Load");
+		CompuwareUEM.enterAction("Android Printer List Load");
 		
 		// Clear the printer list and create the progress dialog
 		printerListFragment.clearList();
