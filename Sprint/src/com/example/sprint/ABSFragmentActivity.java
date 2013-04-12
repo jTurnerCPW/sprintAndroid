08559074b0ecc857e4d9cafca4ff5d6e5fbc85e4
amijzn0 <amijzn0@dtw125408m01.clients.nasa.cpwr.corp>
2013-04-12 09:34:53 -0400

    Updated username dialog. Fixed UEM Scan Time.

--- Sprint/src/com/example/sprint/ABSFragmentActivity.java
+++ Sprint/src/com/example/sprint/ABSFragmentActivity.java
@@ -17,6 +17,7 @@ public class ABSFragmentActivity extends SherlockFragmentActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		CompuwareUEM.leaveAction("Scan Time");
 
 		// initialize actionBar
 		actionBar = getSupportActionBar();
